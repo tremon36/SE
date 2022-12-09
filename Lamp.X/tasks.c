296cca5a887da09444d41b69bc42fb162704aad5
anapetrovx <120026483+anapetrovx@users.noreply.github.com>
2022-12-09 16:28:46 +0100

    Update tasks.c

--- Lamp.X/tasks.c
+++ Lamp.X/tasks.c
@@ -43,5 +43,29 @@ void readNoise(void){
     ADCON0bits.GO=1; // start measurement
 }
 
+void setPWMDuty(void){
+	taskData_set_DutyPWM((taskData_get_DutyPWM()*PR2)/100);
+}
 
-
+char spi_write_read(char one_byte){
+    char x;
+    char answer, x;
+    
+    answer = 0;
+    
+    for(x = 8; x > 0; x--){
+        spi_dat_out = (__bit)((one_byte >> (x - 1)) & 0b00000001);
+		
+        //__delay_us(5);
+        spi_clk = 1;
+		
+        //__delay_us(10);
+        answer |= (char)spi_dat_in;
+        spi_clk = 0;
+		
+        //__delay_us(10);
+        if(x > 1)
+            answer = answer << 1;
+    }
+    return answer;
+}
